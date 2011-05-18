94822b4141cf3088e56407c5e723f692a6babe50
Petr Janda <petrjanda@me.com>
2011-05-18 22:30:01 +0200

    first piece of bin file

--- apps/test/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http'),
-	sys = require('sys');
-
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-})
-
-server.listen(1337, "127.0.0.1");
